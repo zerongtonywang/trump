4ff6893f51721c1eac7bdd69d76fda84a785d5bf
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-05-25 17:47:20 +1000

    small form change

--- no/forms.py
+++ no/forms.py
@@ -6,11 +6,3 @@ class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
         fields = ["quote_text"]
-        labels = {
-            "quote_text": "Your response"
-        }
-        error_messages = {
-            "quote_text": {
-                "required": "Enter a phrase before submitting, how hard is that?"
-            }
-        }
