dd79e2fb6e239c65759b189bfd4f0bff8b0622e7
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-05-25 16:05:19 +1000

    added quote_valid admin action

--- no/admin.py
+++ no/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from .models import Quote
 
 
+def mark_valid(modeladmin, request, queryset):
+    queryset.update(quote_valid=True)
+
+
+def mark_invalid(modeladmin, request, queryset):
+    queryset.update(quote_valid=False)
+
+
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ('quote_text', 'quote_valid')
+    actions = [mark_valid, mark_invalid]
 
 
 admin.site.register(Quote, QuoteAdmin)
